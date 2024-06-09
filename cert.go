3354deaf8fff8757e06b1404486e6c5f5ace9a36
luke <lukemorton@tutanota.com>
2024-06-08 20:38:17 -0500

    fix!: restructured into flat repo

---
+++ cert.go
@@ -0,0 +1,22 @@
+// cert.go
+package cert
+
+import (
+	"fmt"
+)
+
+type Cert struct {
+    Host, Type string
+    Body []byte
+}
+
+func (c *Cert) get() (string, error) {
+    var pathRoot string = "/root/ca/intermediate"
+    if c.Type == "csr" {
+        return (pathRoot + "/csr/" + c.Host + ".csr.pem"), nil
+    } else if c.Type == "cert" {
+        return (pathRoot + "/certs/" + c.Host + ".cert.pem"), nil
+    } else {
+        return "", fmt.Errorf("cert: invalid type '%q'; must be csr or cert", c.Type)
+    }
+}
---
+++ cert_test.go
@@ -0,0 +1,33 @@
+package cert
+
+import "testing"
+
+var testCerts = []*Cert {
+    {Host: "webserver.domain", Body: []byte("Testing ABC"), Type: "csr"},
+    {Host: "appserver.domain", Body: []byte("Testing 123"), Type: "cert"},
+    {Host: "dbserver.domain", Body: []byte("Testing xyz"), Type: "bogus"},
+    {Host: "authserver.domain", Body: []byte("Testing 987"), Type: ""},
+}
+
+type getTest struct {
+    cert *Cert
+    expected string
+}
+
+var getTests = []getTest {
+    {testCerts[0], "/root/ca/intermediate/csr/webserver.domain.csr.pem"},
+    {testCerts[1], "/root/ca/intermediate/certs/appserver.domain.cert.pem"},
+    {testCerts[2], ""},
+    {testCerts[3], ""},
+}
+
+// Cert.get() should output a string containing the filepath
+// /root/ca/intermediate/${Cert.Type}s/${Cert.Host}.${Cert.Type}.pem
+func TestGet(t *testing.T) {
+    for _, test := range getTests {
+        output, _ := test.cert.get()
+        if output != test.expected {
+            t.Errorf("Output %q not equal to expected %q", output, test.expected)
+        }
+    }
+}
