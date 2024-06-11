43d9c21a86ae11c95d294aa5be409f9cd494fd96
luke <lukemorton@tutanota.com>
2024-06-10 19:06:00 -0500

    feat!: added save function for certs

--- cert.go
+++ cert.go
@@ -3,20 +3,103 @@ package main
 
 import (
 	"fmt"
+	"os"
+    "encoding/json"
 )
 
+// configuration file conf.json
+type Configuration struct {
+    PathRoot string `json:"pathRoot"`
+}
+
+/*
+    loadConfig(filename string)
+
+    Loads the configuration file filename in the same path
+    The configuration file should be JSON
+
+    Parameters:
+        filename (string) - the filename of the configuration to import
+
+    Returns:
+        *Configuration -  configuration struct
+        error - if the file cannot be read or deserialized
+
+    Example:
+        c, err = loadConfig("conf.json")
+        fmt.Println(c.Pathroot)
+*/
+func loadConfig(filename string) (*Configuration, error) {
+    body, err := os.ReadFile(filename)
+    if err != nil {
+        return &Configuration{}, fmt.Errorf("loadConfig: failed to read file %q\n%w\n", filename, err)
+    }
+    var c Configuration
+    err = json.Unmarshal(body, &c)
+    if err != nil {
+        return &Configuration{}, fmt.Errorf("loadConfig: failed to parse file %q\n%w\n", filename, err)
+    }
+
+    return &c, nil
+}
+
+// representation of either a csr or cert file 
 type Cert struct {
     Host, Type string
     Body []byte
 }
 
-func (c *Cert) get() (string, error) {
-    var pathRoot string = "/root/ca/intermediate"
+/*
+    Cert.get(config)
+
+    Determines the certificate filepath given the pathRoot in the configuration file
+    
+    Parameters:
+        config (string) - local or full path to the configuration file
+    
+    Returns:
+        string - The full filepath corresponding to the certificate
+        error - If the config input cannot be read
+
+    Example:
+        c, err := myCert.get("conf.json")
+        fmt.Println(c)
+*/
+func (c *Cert) get(config string) (string, error) {
+    conf, err := loadConfig(config)
+    if err != nil {
+        return "", fmt.Errorf("Cert.get: failed to read config file %q\n%w\n", config, err)
+    }
+    var pathRoot string = conf.PathRoot
     if c.Type == "csr" {
         return (pathRoot + "/csr/" + c.Host + ".csr.pem"), nil
     } else if c.Type == "cert" {
         return (pathRoot + "/certs/" + c.Host + ".cert.pem"), nil
     } else {
-        return "", fmt.Errorf("cert: invalid type '%q'; must be csr or cert", c.Type)
+        return "", fmt.Errorf("Cert.get: invalid type %q; must be csr or cert", c.Type)
     }
 }
+
+/*
+    Cert.save(config)
+
+    Saves the certificate content to the appropriate filepath based on the config
+
+    Parameters:
+        config (string) - Local or full path to the config file
+
+    Returns:
+        error - if the config file cannot be read or if the file content cannot be written
+*/
+func (c *Cert) save(config string) error {
+    certPath, err := c.get(config)
+    if err != nil {
+        return fmt.Errorf("Cert.save: failed to get filepath\n%w\n", err)
+    }
+    err = os.WriteFile(certPath, c.Body, 0600)
+    if err != nil {
+        return fmt.Errorf("Cert.save: failed to save file %q\n%w\n", certPath, err)
+    }
+    return nil
+}
+
--- cert_test.go
+++ cert_test.go
@@ -1,6 +1,12 @@
 package main
 
-import "testing"
+import (
+	"bytes"
+	"os"
+	"testing"
+)
+
+var config string = "test_conf.json"
 
 var testCerts = []*Cert {
     {Host: "webserver.domain", Body: []byte("Testing ABC"), Type: "csr"},
@@ -15,8 +21,8 @@ type getTest struct {
 }
 
 var getTests = []getTest {
-    {testCerts[0], "/root/ca/intermediate/csr/webserver.domain.csr.pem"},
-    {testCerts[1], "/root/ca/intermediate/certs/appserver.domain.cert.pem"},
+    {testCerts[0], "./root/ca/intermediate/csr/webserver.domain.csr.pem"},
+    {testCerts[1], "./root/ca/intermediate/certs/appserver.domain.cert.pem"},
     {testCerts[2], ""},
     {testCerts[3], ""},
 }
@@ -25,9 +31,36 @@ var getTests = []getTest {
 // /root/ca/intermediate/${Cert.Type}s/${Cert.Host}.${Cert.Type}.pem
 func TestGet(t *testing.T) {
     for _, test := range getTests {
-        output, _ := test.cert.get()
+        output, _ := test.cert.get(config)
         if output != test.expected {
             t.Errorf("Output %q not equal to expected %q", output, test.expected)
         }
     }
 }
+
+
+// Cert.save() should create a file with the certificate name and file
+func TestSave(t *testing.T) {
+    for _, test := range testCerts {
+        certPath, err := test.get(config)
+        if err != nil {
+            continue
+        }
+        test.save(config)
+
+        // Check that the file exists
+        body, err := os.ReadFile(certPath)
+        if err != nil {
+            t.Errorf("File %q not saved to system", certPath)
+        }
+
+        // Check that the body matches that of the cert struct
+        if !(bytes.Equal(body, test.Body)) {
+            t.Errorf("Body '%q' doesn't match expected body '%q'", body, test.Body)
+        }
+
+        // Cleanup the files
+        os.Remove(certPath)
+    }
+}
+
