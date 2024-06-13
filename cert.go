7eff3059e5bb0a5cd22bd38b95a31e2a0f8c05fd
luke <lukemorton@tutanota.com>
2024-06-12 21:29:54 -0500

    fix: formatted go modules

--- cert.go
+++ cert.go
@@ -3,113 +3,121 @@ package main
 
 import (
 	"fmt"
+	"github.com/pelletier/go-toml/v2"
 	"os"
-    "github.com/pelletier/go-toml/v2"
 )
 
 // configuration file config.toml
 type Configuration struct {
-    CertsRepo string
-    CsrRepo string
+	CertsRepo string
+	CsrRepo   string
 }
 
 /*
-    loadConfig(filename string)
+loadConfig(filename string)
 
-    Loads the configuration file filename in the same path
-    The configuration file should be TOML
+Loads the configuration file filename in the same path
+The configuration file should be TOML
 
-    Parameters:
-        filename (string) - the filename of the configuration to import
+Parameters:
 
-    Returns:
-        *Configuration -  configuration struct
-        error - if the file cannot be read or deserialized
+	filename (string) - the filename of the configuration to import
 
-    Example:
-        c, err = loadConfig("config.toml")
-        fmt.Println(c.Pathroot)
+Returns:
+
+	*Configuration -  configuration struct
+	error - if the file cannot be read or deserialized
+
+Example:
+
+	c, err = loadConfig("config.toml")
+	fmt.Println(c.Pathroot)
 */
 func loadConfig(filename string) (*Configuration, error) {
-    body, err := os.ReadFile(filename)
-    if err != nil {
-        return &Configuration{}, fmt.Errorf("loadConfig: failed to read file %q\n%w\n", filename, err)
-    }
-    var c Configuration
-    err = toml.Unmarshal(body, &c)
-    if err != nil {
-        return &Configuration{}, fmt.Errorf("loadConfig: failed to parse file %q\n%w\n", filename, err)
-    }
+	body, err := os.ReadFile(filename)
+	if err != nil {
+		return &Configuration{}, fmt.Errorf("loadConfig: failed to read file %q\n%w\n", filename, err)
+	}
+	var c Configuration
+	err = toml.Unmarshal(body, &c)
+	if err != nil {
+		return &Configuration{}, fmt.Errorf("loadConfig: failed to parse file %q\n%w\n", filename, err)
+	}
 
-    return &c, nil
+	return &c, nil
 }
 
-// representation of either a csr or cert file 
+// representation of either a csr or cert file
 type Cert struct {
-    Host, Type string
-    Body []byte
+	Host, Type string
+	Body       []byte
 }
 
 /*
-    Cert.get(config)
+Cert.get(config)
 
-    Determines the certificate filepath given the pathRoot in the configuration file
-    
-    Parameters:
-        config (string) - local or full path to the configuration file
-    
-    Returns:
-        string - The full filepath corresponding to the certificate
-        error - If the config input cannot be read
+# Determines the certificate filepath given the pathRoot in the configuration file
 
-    Example:
-        c, err := myCert.get("conf.json")
-        fmt.Println(c)
+Parameters:
+
+	config (string) - local or full path to the configuration file
+
+Returns:
+
+	string - The full filepath corresponding to the certificate
+	error - If the config input cannot be read
+
+Example:
+
+	c, err := myCert.get("conf.json")
+	fmt.Println(c)
 */
 func (c *Cert) get(config *Configuration) (string, error) {
-    if c.Type == "csr" {
-        return (config.CsrRepo + c.Host + ".csr.pem"), nil
-    } else if c.Type == "cert" {
-        return (config.CertsRepo + c.Host + ".cert.pem"), nil
-    } else {
-        return "", fmt.Errorf("Cert.get: invalid type %q; must be csr or cert", c.Type)
-    }
+	if c.Type == "csr" {
+		return (config.CsrRepo + c.Host + ".csr.pem"), nil
+	} else if c.Type == "cert" {
+		return (config.CertsRepo + c.Host + ".cert.pem"), nil
+	} else {
+		return "", fmt.Errorf("Cert.get: invalid type %q; must be csr or cert", c.Type)
+	}
 }
 
 /*
-    Cert.save(config)
+Cert.save(config)
 
-    Saves the certificate content to the appropriate filepath based on the config
+# Saves the certificate content to the appropriate filepath based on the config
 
-    Parameters:
-        config (string) - Local or full path to the config file
+Parameters:
 
-    Returns:
-        error - if the config file cannot be read or if the file content cannot be written
+	config (string) - Local or full path to the config file
+
+Returns:
+
+	error - if the config file cannot be read or if the file content cannot be written
 */
 func (c *Cert) save(config *Configuration) error {
-    certPath, err := c.get(config)
-    if err != nil {
-        return fmt.Errorf("Cert.save: failed to get filepath\n%w\n", err)
-    }
+	certPath, err := c.get(config)
+	if err != nil {
+		return fmt.Errorf("Cert.save: failed to get filepath\n%w\n", err)
+	}
 
-    var pathRoot string
-    if c.Type == "csr" {
-        pathRoot = config.CsrRepo
-    } else {
-        pathRoot = config.CertsRepo
-    }
-    
-    _, err = os.Stat(pathRoot)
-    if os.IsNotExist(err) {
-        err = os.MkdirAll(pathRoot, os.ModePerm)
-        if err != nil {
-            return fmt.Errorf("Cert.save: failed to create path %q\n%w\n", pathRoot, err)
-        }
-    }
-    err = os.WriteFile(certPath, c.Body, 0444)
-    if err != nil {
-        return fmt.Errorf("Cert.save: failed to save file %q\n%w\n", certPath, err)
-    }
-    return nil
+	var pathRoot string
+	if c.Type == "csr" {
+		pathRoot = config.CsrRepo
+	} else {
+		pathRoot = config.CertsRepo
+	}
+
+	_, err = os.Stat(pathRoot)
+	if os.IsNotExist(err) {
+		err = os.MkdirAll(pathRoot, os.ModePerm)
+		if err != nil {
+			return fmt.Errorf("Cert.save: failed to create path %q\n%w\n", pathRoot, err)
+		}
+	}
+	err = os.WriteFile(certPath, c.Body, 0444)
+	if err != nil {
+		return fmt.Errorf("Cert.save: failed to save file %q\n%w\n", certPath, err)
+	}
+	return nil
 }
--- cert_test.go
+++ cert_test.go
@@ -6,83 +6,82 @@ import (
 	"testing"
 )
 
-var testCerts = []*Cert {
-    {Host: "webserver.domain", Body: []byte("Testing ABC"), Type: "csr"},
-    {Host: "appserver.domain", Body: []byte("Testing 123"), Type: "cert"},
-    {Host: "dbserver.domain", Body: []byte("Testing xyz"), Type: "bogus"},
-    {Host: "authserver.domain", Body: []byte("Testing 987"), Type: ""},
+var testCerts = []*Cert{
+	{Host: "webserver.domain", Body: []byte("Testing ABC"), Type: "csr"},
+	{Host: "appserver.domain", Body: []byte("Testing 123"), Type: "cert"},
+	{Host: "dbserver.domain", Body: []byte("Testing xyz"), Type: "bogus"},
+	{Host: "authserver.domain", Body: []byte("Testing 987"), Type: ""},
 }
 
 type getTest struct {
-    cert *Cert
-    expected string
+	cert     *Cert
+	expected string
 }
 
-var getTests = []getTest {
-    {testCerts[0], "./root/ca/intermediate/csr/webserver.domain.csr.pem"},
-    {testCerts[1], "./root/ca/intermediate/certs/appserver.domain.cert.pem"},
-    {testCerts[2], ""},
-    {testCerts[3], ""},
+var getTests = []getTest{
+	{testCerts[0], "./root/ca/intermediate/csr/webserver.domain.csr.pem"},
+	{testCerts[1], "./root/ca/intermediate/certs/appserver.domain.cert.pem"},
+	{testCerts[2], ""},
+	{testCerts[3], ""},
 }
 
 // loadConfig() should output a configuration for the test config
 func TestLoadConfig(t *testing.T) {
-    config, err := loadConfig("test.toml")
-    if err != nil {
-        t.Errorf("loadConfig failed due to error %q", err)
-    }
-    
-    var certsExpected string = "./root/ca/intermediate/certs/"
-    var csrExpected string = "./root/ca/intermediate/csr/"
+	config, err := loadConfig("test.toml")
+	if err != nil {
+		t.Errorf("loadConfig failed due to error %q", err)
+	}
 
-    if config.CertsRepo != certsExpected {
-        t.Errorf("Output %q not equal to expected %q", config.CertsRepo, certsExpected)
-    }
+	var certsExpected string = "./root/ca/intermediate/certs/"
+	var csrExpected string = "./root/ca/intermediate/csr/"
 
-    if config.CsrRepo != csrExpected {
-        t.Errorf("Output %q not equal to expected %q", config.CsrRepo, csrExpected)
-    }
+	if config.CertsRepo != certsExpected {
+		t.Errorf("Output %q not equal to expected %q", config.CertsRepo, certsExpected)
+	}
+
+	if config.CsrRepo != csrExpected {
+		t.Errorf("Output %q not equal to expected %q", config.CsrRepo, csrExpected)
+	}
 }
 
 // Cert.get() should output a string containing the filepath
 // /root/ca/intermediate/${Cert.Type}s/${Cert.Host}.${Cert.Type}.pem
 func TestGet(t *testing.T) {
-    config, _ := loadConfig("test.toml")
-    for _, test := range getTests {
-        output, _ := test.cert.get(config)
-        if output != test.expected {
-            t.Errorf("Output %q not equal to expected %q", output, test.expected)
-        }
-    }
+	config, _ := loadConfig("test.toml")
+	for _, test := range getTests {
+		output, _ := test.cert.get(config)
+		if output != test.expected {
+			t.Errorf("Output %q not equal to expected %q", output, test.expected)
+		}
+	}
 }
-
 
 // Cert.save() should create a file with the certificate name and file
 func TestSave(t *testing.T) {
-    config, _ := loadConfig("test.toml")
-    for _, test := range testCerts {
-        certPath, err := test.get(config)
-        if err != nil {
-            continue
-        }
+	config, _ := loadConfig("test.toml")
+	for _, test := range testCerts {
+		certPath, err := test.get(config)
+		if err != nil {
+			continue
+		}
 
-        err = test.save(config)
-        if err != nil {
-            t.Errorf("Cert.save produced error: %q", err)
-        }
+		err = test.save(config)
+		if err != nil {
+			t.Errorf("Cert.save produced error: %q", err)
+		}
 
-        // Check that the file exists
-        body, err := os.ReadFile(certPath)
-        if err != nil {
-            t.Errorf("File %q not saved to system", certPath)
-        }
+		// Check that the file exists
+		body, err := os.ReadFile(certPath)
+		if err != nil {
+			t.Errorf("File %q not saved to system", certPath)
+		}
 
-        // Check that the body matches that of the cert struct
-        if !(bytes.Equal(body, test.Body)) {
-            t.Errorf("Body '%q' doesn't match expected body '%q'", body, test.Body)
-        }
+		// Check that the body matches that of the cert struct
+		if !(bytes.Equal(body, test.Body)) {
+			t.Errorf("Body '%q' doesn't match expected body '%q'", body, test.Body)
+		}
 
-        // Cleanup the files
-        os.Remove(certPath)
-    }
+		// Cleanup the files
+		os.Remove(certPath)
+	}
 }
